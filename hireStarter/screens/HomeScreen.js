e50bf895727c54c378635c5331128957a641fe13
Ricardo Kuchimpos <rkuchimpos@live.com>
2019-11-09 18:46:35 -0800

    Add navigation between home and profile page

--- hireStarter/screens/HomeScreen.js
+++ hireStarter/screens/HomeScreen.js
@@ -2,11 +2,16 @@ import React from "react";
 import { Button, Text } from "react-native";
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: "hireStarter",
-    headerRight: () => (
-      <Button onPress={() => alert("Button placeholder")} title="placeholder" />
-    )
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "hireStarter",
+      headerRight: () => (
+        <Button
+          onPress={() => navigation.navigate("Profile")}
+          title="profile"
+        />
+      )
+    };
   };
   render() {
     return <Text>HomeScreen</Text>;
--- hireStarter/screens/ProfileScreen.js
+++ hireStarter/screens/ProfileScreen.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Text } from 'react-native';
 
 class ProfileScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "My Profile",
+    };
+  };
   render() {
     return (
       <Text>ProfileScreen</Text>
