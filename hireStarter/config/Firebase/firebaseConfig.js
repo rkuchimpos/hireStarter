2f951c44da2c1c2a3dc7ed081a9a6e467803145b
rtphan <rtphan@g.ucla.edu>
2019-11-14 11:33:26 -0800

    Firebase Setup

---
+++ hireStarter/config/Firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: 'AIzaSyDaxeMiG93B7EVNFgcnK0kjWysZEwtdAjg',
+    authDomain: 'hirestarter-bb5dd.firebaseapp.com',
+    databaseURL: 'https://hirestarter-bb5dd.firebaseio.com/',
+    projectId: 'hirestarter-bb5dd',
+    storageBucket: 'hirestarter-bb5dd.appspot.com',
+    appId: '1:1071020312462:web:92f2f315207139d24cc6df'
+}
---
+++ hireStarter/navigation/AppContainer.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { createAppContainer } from "react-navigation"
+import { createStackNavigator } from "react-navigation-stack"
+import { LinearGradient } from "expo-linear-gradient"
+import HomeScreen from "./screens/HomeScreen"
+import EditProfileScreen from "./screens/EditProfileScreen"
+import MatchesScreen from "./screens/MatchesScreen"
+import LoginScreen from "./screens/LoginScreen"
+import ViewProfileScreen from "./screens/ViewProfileScreen"
+
+const MainNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen
+    },
+    EditProfile: {
+      screen: EditProfileScreen
+    },
+    Matches: {
+      screen: MatchesScreen
+    },
+    Login: {
+      screen: LoginScreen
+    },
+    ViewProfile: {
+      screen: ViewProfileScreen,
+    }
+  },
+  {
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      headerBackground: (
+        <LinearGradient
+          style={{ flex: 1 }}
+          colors={["#8E2DE2", "#4A00E0"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+        />
+      ),
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontWeight: "normal"
+      }
+    }
+  }
+);
+
+const AppContainer = createAppContainer(MainNavigator);
+export default AppContainer;
