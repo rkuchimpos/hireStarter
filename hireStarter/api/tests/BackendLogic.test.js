ef80b1924de3f48ba666b5918b1170a0889487a2
Ricardo Kuchimpos <rkuchimpos@live.com>
2019-11-14 23:18:34 -0800

    Add like test

--- hireStarter/api/tests/BackendLogic.test.js
+++ hireStarter/api/tests/BackendLogic.test.js
@@ -5,3 +5,22 @@ test('retrieving profile by uid should return matching profile', () => {
   const profile = BackendLogic.fetchCardByUID(expectedUid);
   expect(profile.uid).toBe(expectedUid)
 });
+
+// TODO: Replace mock calls with actual calls
+test('two mutual likes should be recognized as a match', () => {
+  const user1 = 1;
+  const user2 = 2;
+  BackendLogic.addLikeMock(user1, user2);
+  BackendLogic.addLikeMock(user2, user1);
+  const isMatch = Backend.checkConnectionMock(user1, user2);
+  expect(isMatch).toBe(true);
+});
+
+// TODO: Replace mock calls with actual calls
+test('unreciprocated match should not be recognized as a match', () => {
+  const user1 = 3;
+  const user2 = 4;
+  BackendLogic.addLikeMock(user1, user2);
+  const isMatch = Backend.checkConnectionMock(user1, user2);
+  expect(isMatch).toBe(false);
+});
