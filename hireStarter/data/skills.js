df98faccc9d24481bf152747ef7a03bf38a5a38e
Ricardo Kuchimpos <rkuchimpos@live.com>
2019-11-30 17:43:37 -0800

    Create list of skills to choose from

---
+++ hireStarter/data/skills.js
@@ -0,0 +1,25 @@
+/*
+ List of skills to choose from.
+ Import using: `import { skills } from "../data/skills";`
+ */
+export const skills = [
+  "Software Engineering",
+  "Computer Vision",
+  "Machine Learning",
+  "Artificial Intelligence",
+  "Distributed Computing",
+  "Front-End Development",
+  "Back-End Development",
+  "Full-Stack Development",
+  "Embedded Systems",
+  "iOS",
+  "Android",
+  "Data Science",
+  "Data Mining",
+  "Computer Graphics",
+  "Databases",
+  "Linux",
+  "QA & Testing",
+  "Game Development",
+  "Virtual Reality"
+];
