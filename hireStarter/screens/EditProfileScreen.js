aee3ff9aa3c537057528ef08573e1c899809818d
Ricardo Kuchimpos <rkuchimpos@live.com>
2019-12-01 17:11:56 -0800

    Use keyboard aware layout for edit profile page

--- hireStarter/screens/EditProfileScreen.js
+++ hireStarter/screens/EditProfileScreen.js
@@ -1,52 +1,61 @@
 import React from "react";
-import { Dimensions, Image, StyleSheet, Text, TextInput, View, TouchableOpacity } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
-import UserProfile from '../models/UserProfile';
-import BackendAPI from '../api/BackendAPI';
-import { withFirebaseHOC, ProfileAPI } from '../config/Firebase'
+import {
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import UserProfile from "../models/UserProfile";
+import BackendAPI from "../api/BackendAPI";
+import { withFirebaseHOC, ProfileAPI } from "../config/Firebase";
 
 const { width } = Dimensions.get("window");
 
 function titleCase(string) {
   var sentence = string.toLowerCase().split(" ");
-  for(var i = 0; i< sentence.length; i++){
-     sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+  for (var i = 0; i < sentence.length; i++) {
+    sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
   }
   return sentence.join(" ");
 }
 
 class EditProfileScreen extends React.Component {
   constructor(props) {
-    super(props)
-    this.navigation = this.props.navigation
+    super(props);
+    this.navigation = this.props.navigation;
     this.state = {
-      city: '',
+      city: "",
       connections: [],
-      description: '',
-      email: '',
+      description: "",
+      email: "",
       image1: "https://retohercules.com/images/transparent-to-the-user-8.png",
       image2: "https://retohercules.com/images/transparent-to-the-user-8.png",
-      name: '',
-      organization: '',
+      name: "",
+      organization: "",
       potentials: [],
       recruiter: false,
       skills: [],
-      uid: this.navigation.getParam('uid', 'NO-UID')
+      uid: this.navigation.getParam("uid", "NO-UID")
     };
-    this.newUser = this.navigation.getParam('newUser', false)
+    this.newUser = this.navigation.getParam("newUser", false);
     if (this.newUser) {
-      this.state.email = this.navigation.getParam('email')
-      this.state.name = titleCase(this.navigation.getParam('name'))
+      this.state.email = this.navigation.getParam("email");
+      this.state.name = titleCase(this.navigation.getParam("name"));
     }
   }
 
   // Loads data from Firestore here
   componentDidMount() {
-    console.log(this.state)
+    console.log(this.state);
     if (!this.newUser) {
-      ProfileAPI.getUserData(this.state.uid).then((result) => {
-        this.setState(result)
-      })
+      ProfileAPI.getUserData(this.state.uid).then(result => {
+        this.setState(result);
+      });
     }
   }
 
@@ -56,9 +65,9 @@ class EditProfileScreen extends React.Component {
    */
   updateUserData() {
     if (this.newUser) {
-      ProfileAPI.createNewUser(this.state)
+      ProfileAPI.createNewUser(this.state);
     } else {
-      ProfileAPI.updateUserData(this.state)
+      ProfileAPI.updateUserData(this.state);
     }
   }
 
@@ -69,23 +78,23 @@ class EditProfileScreen extends React.Component {
   };
 
   changeDescription(value) {
-    this.setState({description: value});
+    this.setState({ description: value });
     //myUserProfile.description = value;
     //this.updateUserData() // Please make a submit button to call this
   }
 
   changeName(value) {
-    this.setState({name: value});
+    this.setState({ name: value });
     //this.updateUserData();
   }
 
   changeEmail(value) {
-    this.setState({email: value});
+    this.setState({ email: value });
     //this.updateUserData();
   }
 
   changeCity(value) {
-    this.setState({city: value});
+    this.setState({ city: value });
     //this.updateUserData();
   }
 
@@ -99,70 +108,95 @@ class EditProfileScreen extends React.Component {
 
     if (!result.cancelled) {
       if (pic_num == 1) {
-        console.log(result)
-        this.setState({image1: await ProfileAPI.uploadImage(this.state.uid, 1, result.uri)});
+        console.log(result);
+        this.setState({
+          image1: await ProfileAPI.uploadImage(this.state.uid, 1, result.uri)
+        });
       } else if (pic_num == 2) {
-        this.setState({image2: await ProfileAPI.uploadImage(this.state.uid, 2, result.uri)});
+        this.setState({
+          image2: await ProfileAPI.uploadImage(this.state.uid, 2, result.uri)
+        });
       }
     }
-  };
+  }
 
   render() {
     let { image1, image2 } = this.state;
 
     return (
-      <View style={styles.container}>
-        <View style={{flexDirection: "row"}}>
-          <TouchableOpacity style={{flex: 1, height: width / 2, width: width / 2, borderWidth: 1, borderColor: '#000000'}} onPress={() => {
-            this.pickImage(1);}}>
-            {image1 &&
-            <Image source={{ uri: image1 }} style={styles.profilePhoto}/>}
+      <KeyboardAvoidingView behavior="position" style={styles.container}>
+        <View style={{ flexDirection: "row" }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              height: width / 2,
+              width: width / 2,
+              borderWidth: 1,
+              borderColor: "#000000"
+            }}
+            onPress={() => {
+              this.pickImage(1);
+            }}
+          >
+            {image1 && (
+              <Image source={{ uri: image1 }} style={styles.profilePhoto} />
+            )}
           </TouchableOpacity>
 
-          <TouchableOpacity style={{height: width / 2, width: width / 2, borderWidth: 1, borderColor: '#000000'}} onPress={() => {
-            this.pickImage(2);}}>
-            {image2 &&
-            <Image source={{ uri: image2 }} style={styles.profilePhoto}/>}
+          <TouchableOpacity
+            style={{
+              height: width / 2,
+              width: width / 2,
+              borderWidth: 1,
+              borderColor: "#000000"
+            }}
+            onPress={() => {
+              this.pickImage(2);
+            }}
+          >
+            {image2 && (
+              <Image source={{ uri: image2 }} style={styles.profilePhoto} />
+            )}
           </TouchableOpacity>
         </View>
         <Text style={styles.categoryHeader}>Name</Text>
-        <View style={{backgroundColor: "#fff"}}>
+        <View style={{ backgroundColor: "#fff" }}>
           <TextInput
             style={styles.TextInputField}
             multiline
             placeholder="Tell us your name!"
             value={this.state.name}
-            onChangeText={(value) => this.changeName(value)}
+            onChangeText={value => this.changeName(value)}
           />
         </View>
         <Text style={styles.categoryHeader}>Email</Text>
-        <View style={{backgroundColor: "#fff"}}>
+        <View style={{ backgroundColor: "#fff" }}>
           <TextInput
             style={styles.TextInputField}
             multiline
             placeholder="Enter your email!"
             value={this.state.email}
-            onChangeText={(value) => this.changeEmail(value)}
+            onChangeText={value => this.changeEmail(value)}
           />
         </View>
         <Text style={styles.categoryHeader}>City</Text>
-        <View style={{backgroundColor: "#fff"}}>
+        <View style={{ backgroundColor: "#fff" }}>
           <TextInput
             style={styles.TextInputField}
             multiline
             placeholder="Enter your location!"
             value={this.state.city}
-            onChangeText={(value) => this.changeCity(value)}
+            onChangeText={value => this.changeCity(value)}
           />
         </View>
         <Text style={styles.categoryHeader}>About Me</Text>
-        <View style={{backgroundColor: "#fff"}}>
+        <View style={{ backgroundColor: "#fff" }}>
           <TextInput
             style={styles.TextInputField}
             multiline
             placeholder="Tell us about yourself!"
             value={this.state.description}
-            onChangeText={(value) => this.changeDescription(value)}
+            onChangeText={value => this.changeDescription(value)}
           />
         </View>
 
@@ -175,7 +209,7 @@ class EditProfileScreen extends React.Component {
         >
           <Text style={styles.text}> Save Changes </Text>
         </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -187,13 +221,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#e5e5e5"
   },
   profilePhoto: {
-    width: (width / 2) - 2,
-    height: (width / 2) - 2
+    width: width / 2 - 2,
+    height: width / 2 - 2
   },
   categoryHeader: {
     fontSize: 20,
     color: "#414141",
-    margin: 10,
+    margin: 10
   },
   TextInputField: {
     marginHorizontal: 15,
