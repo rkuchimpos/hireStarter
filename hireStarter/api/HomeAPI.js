5bfb53088d3b721438f995230bd745a70e0d8d40
rtphan <rtphan@g.ucla.edu>
2019-12-01 21:36:10 -0800

    Format Fixes

--- hireStarter/api/HomeAPI.js
+++ hireStarter/api/HomeAPI.js
@@ -3,91 +3,78 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const HomeAPI = {
-    /**
-     * Add a like to the card that was swiped right.
-     *
-     * @param {String} id1 The user_id of swiping user.
-     * @param {String} id2 The user_id that user swiped right on.
-     * @return {Boolean} Whether API call succeeded.
-     */
-    addPotential: async (id1, id2) => {
-        status = await firebase.firestore().collection('users').doc(`${id1}`).update({
-            potentials: firebase.firestore.FieldValue.arrayUnion(`${id2}`)
-        });
-        return true;
-    },
+	/**
+	 * Add a like to the card that was swiped right.
+	 *
+	 * @param {String} id1 The user_id of swiping user.
+	 * @param {String} id2 The user_id that user swiped right on.
+	 * @return {Boolean} Whether API call succeeded.
+	 */
+	addPotential: async (id1, id2) => {
+		status = await firebase.firestore().collection('users').doc(`${id1}`).update({
+			potentials: firebase.firestore.FieldValue.arrayUnion(`${id2}`)
+		});
+		return true;
+	},
 
-    /**
-     * Check if a user has a connection (mutual like).
-     *
-     * @param {String} user_id1 The main user.
-     * @param {String} user_id2 The user_id2 potentially in connection with user_id1.
-     * @return {Boolean} Whether API call succeeded.
-     */
-    checkConnection: async (user_id1, user_id2) => {
-        collection = firebase.firestore().collection('users')
-        query = await collection.doc(`${user_id2}`).get()
-        potential_list = query.data().potentials
-        if (potential_list.includes(user_id1)){
-            return true;
-        }
-        return false;
-    },
+	/**
+	 * Check if a user has a connection (mutual like).
+	 *
+	 * @param {String} user_id1 The main user.
+	 * @param {String} user_id2 The user_id2 potentially in connection with user_id1.
+	 * @return {Boolean} Whether API call succeeded.
+	 */
+	checkConnection: async (user_id1, user_id2) => {
+		collection = firebase.firestore().collection('users')
+		query = await collection.doc(`${user_id2}`).get()
+		potential_list = query.data().potentials
+		if (potential_list.includes(user_id1)) {
+			return true;
+		}
+		return false;
+	},
 
-    /**
-    * Save that a user has established a connection (mutual like).
-    *
-    * @param {String} id1 The main user.
-    * @param {String} id2 The user_ids th
-    * @return {Boolean} Whether API call succeeded.
-    */
-    addMatches: async (id1, id2) => {
-        collection = firebase.firestore().collection('users')
-        status1 = await firebase.firestore().collection('users').doc(id1).update({
-            connections: firebase.firestore.FieldValue.arrayUnion(`${id2}`)
-        });
-        status2 = await firebase.firestore().collection('users').doc(id2).update({
-            connections: firebase.firestore.FieldValue.arrayUnion(`${id1}`)
-        });
-        return true;
-    },
+	/**
+	* Save that a user has established a connection (mutual like).
+	*
+	* @param {String} id1 The main user.
+	* @param {String} id2 The user_ids th
+	* @return {Boolean} Whether API call succeeded.
+	*/
+	addMatches: async (id1, id2) => {
+		collection = firebase.firestore().collection('users')
+		status1 = await firebase.firestore().collection('users').doc(id1).update({
+			connections: firebase.firestore.FieldValue.arrayUnion(`${id2}`)
+		});
+		status2 = await firebase.firestore().collection('users').doc(id2).update({
+			connections: firebase.firestore.FieldValue.arrayUnion(`${id1}`)
+		});
+		return true;
+	},
 
-    getFirstPhoto: async id => {
-        user_doc = firebase.firestore().collection('users').doc(id).get()
-        return user_doc.data().image1
-    },
-    /**
-     * Retrieve all data relevant to the Card class
-     *
-     * @param {string} uid The user's unique identifier.
-     * @return {Object} A formatted JSON object that describes the user.
-     */
-    fetchCards: async (uid) => {
-        collection = firebase.firestore().collection('users')
-        own_profile  = await collection.doc(`${uid}`).get()
-        own_data = own_profile.data()
-        prof_type = own_data.recruiter;
-        likes = own_data.potentials;
-        docs = await collection.get();
-        
-        card_ids = [];
-        docs.forEach(doc => {
-          const data = doc.data()
-          if (! likes.includes(data.uid) && (data.recruiter != prof_type)){
-            card_ids.push(data.uid)
-          }
-        });
-        // collection.get().then(function (querySnapshot){
-        //     querySnapshot.forEach(function(doc){
-        //         data = doc.data()
-        //         // if (! likes.includes(doc.uid) && (doc.recruiter !=  prof_type)){
-        //         //     card_ids.push(doc.uid);
-        //         // }
-        //         card_ids.push(data.uid)
-        //     });
-        // });
-        return card_ids; 
-    }    
+	/**
+	 * Retrieve all data relevant to the Card class
+	 *
+	 * @param {string} uid The user's unique identifier.
+	 * @return {Object} A formatted JSON object that describes the user.
+	 */
+	fetchCards: async (uid) => {
+		collection = firebase.firestore().collection('users')
+		own_profile = await collection.doc(`${uid}`).get()
+		own_data = own_profile.data()
+		prof_type = own_data.recruiter;
+		likes = own_data.potentials;
+		docs = await collection.get();
+
+		card_ids = [];
+		docs.forEach(doc => {
+			const data = doc.data()
+			if (!likes.includes(data.uid) && (data.recruiter != prof_type)) {
+				card_ids.push(data.uid)
+			}
+		});
+		return card_ids;
+	}
 }
 
 export default HomeAPI
